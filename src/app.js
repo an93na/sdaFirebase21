aa2cfe959f4473da8d12b51881e85da6c9c37d51
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-12 15:47:07 +0100

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // import { getStorage, uploadBytes } from "firebase/storage";
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, getStorage, listAll, ref, uploadBytes } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,59 +23,59 @@ const storage = getStorage(app);
 
 
 
-const url = "https://firebasestorage.googleapis.com/v0/b/moj121frontpol.appspot.com/o/IMG_20201122_101754.jpg?alt=media&token=6cf101bc-283b-4815-a7c2-1cddf9d7c995"
+// const url = "https://firebasestorage.googleapis.com/v0/b/moj121frontpol.appspot.com/o/IMG_20201122_101754.jpg?alt=media&token=6cf101bc-283b-4815-a7c2-1cddf9d7c995"
 
-const obrazek = document.createElement("img");
-obrazek.setAttribute("src", url);  //img.src =
-document.body.appendChild(obrazek);
+// const obrazek = document.createElement("img");
+// obrazek.setAttribute("src", url);  //img.src =
+// document.body.appendChild(obrazek);
 
-const link = "https://reqres.in/api/users?page=2"
-fetch(link)
-.then((daneZPromisa) => daneZPromisa.json())
-.then((danezJSON) => console.log(danezJSON.data));
-
-
-async function myFunc () {
-  const data = await fetch("https://reqres.in/api/users")
-  const users = await data.json();
-  console.log(users.data);
-}
-
-myFunc();
+// const link = "https://reqres.in/api/users?page=2"
+// fetch(link)
+// .then((daneZPromisa) => daneZPromisa.json())
+// .then((danezJSON) => console.log(danezJSON.data));
 
 
+// async function myFunc () {
+//   const data = await fetch("https://reqres.in/api/users")
+//   const users = await data.json();
+//   console.log(users.data);
+// }
 
-const headerInfo = document.getElementById("myHeader");
-const mojButton = document.getElementById("myButton");
-const tekstUzytkownika = document.getElementById("tekstUzytkownika")
-const img = document.createElement("img");
+// myFunc();
 
-mojButton.addEventListener("click", () => {
-  //console.log('Kliknięto przcisk')
-    headerInfo.innerText= "Przesyłam..."
-    const file = document.getElementById("myFile").files[0];
-    let fileName = file.name;
 
-    if(tekstUzytkownika.value){
-      fileName = tekstUzytkownika.value
-    }
-    const imageRef = ref(storage, fileName);
+
+// const headerInfo = document.getElementById("myHeader");
+// const mojButton = document.getElementById("myButton");
+// const tekstUzytkownika = document.getElementById("tekstUzytkownika")
+// const img = document.createElement("img");
+
+// mojButton.addEventListener("click", () => {
+//   //console.log('Kliknięto przcisk')
+//     headerInfo.innerText= "Przesyłam..."
+//     const file = document.getElementById("myFile").files[0];
+//     let fileName = file.name;
+
+//     if(tekstUzytkownika.value){
+//       fileName = tekstUzytkownika.value
+//     }
+//     const imageRef = ref(storage, fileName);
     
     
 
-    uploadBytes(imageRef, file).then(() => {
-      console.log("Sukces!");
-      headerInfo.innerText= "Przesłano"
-      
-      getDownloadURL(imageRef).then(url => {  
-      img.src = url;
-      img.style.width = "250px";
-      document.body.appendChild(img);
-    })
+//     uploadBytes(imageRef, file).then(() => {
+//       console.log("Sukces!");
+//       headerInfo.innerText= "Przesłano"
 
-    })
-  }
-) 
+//       getDownloadURL(imageRef).then(url => {  
+//       img.src = url;
+//       img.style.width = "250px";
+//       document.body.appendChild(img);
+//     })
+
+//     })
+//   }
+// ) 
 
 //1. Dodac Input do podawania nazwy obrazka
 //2. Dodac przycisk do wyswietlania obrazka
@@ -103,4 +103,37 @@ mojButton.addEventListener("click", () => {
 // )
 
 
+// const storageRef = ref(storage);
+// listAll(storageRef).then(res => {
+//   for (let i = 0; i<res.items.length; i++){
+//     console.log(res.items[i].name)
+//   }
+// });
 
+// const storageRef = ref(storage);
+// listAll(storageRef).then(res => {
+//   const Ola = document.getElementById("photoList");// Pobieramy getElementById - element OL
+//   for (let i = 0; i < res.items.length; i++) {
+//      let newOla = document.createElement("li"); // tworzymy nowy lement li - createElement
+//      newOla.innerText = res.items[i].name // w elemencie li ustawiamy tekst na nazwe pliku innerText / res.items[i].name
+//      Ola.appendChild(newOla)  // li dodajemy do ol - appendChild  
+//   }
+// });
+
+const storageRef = ref(storage);
+listAll(storageRef).then(res => {
+  const Ola = document.getElementById("photoList");
+  
+  res.items.forEach(item => {
+    let newOla = document.createElement("li");
+    const przycisk = document.createElement("button");
+
+    przycisk.addEventListener("click", () => console.log("Siemanko"));
+
+    przycisk.innerText= "Naciśnij Proszę"
+    newOla.innerText = item.name;
+
+    Ola.appendChild(newOla);
+    newOla.appendChild(przycisk);
+  });
+});
