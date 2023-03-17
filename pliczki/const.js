d5969542ed25ae7dbc6ba300ab228e5adb8a7bf3
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-17 20:42:54 +0100

    changes

--- pliczki/const.js
+++ pliczki/const.js
@@ -1,6 +1,8 @@
 export const bodySelector = 'body';
 
-export const button = document.createElement('button');
-document.body.appendChild(button);
-button.innerText = "Klik";
+export function createButton() {
+    const button = document.createElement('button');
+    document.body.appendChild(button);
+    button.innerText = "Klik";
+}
 
