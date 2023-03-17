3c5cf836544c76434ebd2019d5b6f575aff1a096
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-17 20:08:19 +0100

    changes

--- pliczki/pierwszy.js
+++ pliczki/pierwszy.js
@@ -1,4 +1,6 @@
 import { bodySelector } from "./const.js";
+import { isEmpty } from "./utils.js";
 
 // console.log("Test 1, 2, 3");
-console.log(bodySelector);
+// console.log(bodySelector);
+console.log(isEmpty([]));
---
+++ pliczki/utils.js
@@ -0,0 +1,10 @@
+export function isEmpty(arr) {
+    return !arr || arr.length === 0;
+}
+
+// export function isEmpty(arr) {
+//     if(arr){
+//         return arr.length === 0;
+//     }
+//     return true;
+// }
