81d882da7b6d58525e3ab5c2e3192f14d816b6c9
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-18 20:43:38 +0100

    changes

--- nauka/src/app.js
+++ nauka/src/app.js
@@ -1,3 +1,15 @@
 import './../styles/styles.css';
 
-console.log("czy działam");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXJWy6s_1p1qdH7qDvwUdo4wNZBMg1ASA",
+  authDomain: "naukamoja-50dd4.firebaseapp.com",
+  projectId: "naukamoja-50dd4",
+  storageBucket: "naukamoja-50dd4.appspot.com",
+  messagingSenderId: "149285652768",
+  appId: "1:149285652768:web:a14fd357e02ddc9e08ef45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
